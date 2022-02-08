bd870f43ea211d70efd32e5420dd5570789ae2d1
optionallychained <6907442+optionallychained@users.noreply.github.com>
2022-02-08 18:05:29 +0000

    freecam geometry demo

---
+++ src/entity/axis.ts
@@ -0,0 +1,34 @@
+import { Angle, Color, Component, Entity, Geometry, Shader, Vec3 } from 'aura';
+
+export class Axis extends Entity.Entity {
+
+    constructor(axis: 'x' | 'y' | 'z', length: number) {
+        let angleX, angleY, angleZ;
+
+        switch (axis) {
+            case 'x':
+                angleX = angleY = angleZ = 0;
+                break;
+
+            case 'y':
+                angleX = angleY = 0;
+                angleZ = Angle.toRadians(90);
+                break;
+
+            case 'z':
+                angleX = angleZ = 0;
+                angleY = Angle.toRadians(90);
+                break;
+        }
+
+        super({
+            tag: 'axis',
+            components: [
+                new Component.ThreeD.Transform3D(new Vec3(), new Vec3(length, 1, 1), new Vec3(angleX, angleY, angleZ)),
+                new Component.Generic.Model(Geometry.ThreeD.LINE),
+                new Component.Generic.Shader(Shader.Program.ThreeD.PROGRAM_BASIC_3D),
+                new Component.Generic.FlatColor(Color.white()),
+            ]
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { Core, Vec2 } from 'aura';
+import { Core, Shader, Vec2, Vec3 } from 'aura';
 import { MAIN_STATE } from './state/main.state';
 
 const game = new Core.ThreeD.Game3D({
-    canvasDimensions: new Vec2(1024, 768)
+    canvasDimensions: new Vec2(1024, 768),
+    world: {
+        dimensions: new Vec3(1024 * 100, 768 * 100, 1000000)
+    }
 });
+
+game.registerShader(Shader.Program.ThreeD.PROGRAM_BASIC_3D);
+game.registerShader(Shader.Program.ThreeD.PROGRAM_COLOR_PER_VERTEX_3D);
 
 game.addState(MAIN_STATE);
 
