edeb0a04ba292b301be1d05df3e5b71cda1ec63a
optionallychained <6907442+optionallychained@users.noreply.github.com>
2022-06-09 10:19:22 +0100

    upgrade Aura

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { Game, ShaderPrograms, Vec2, Vec3 } from 'aura-3d';
+import { Game, Vec2, Vec3 } from 'aura-3d';
 import { MAIN_STATE } from './state/main.state';
 
 const game = new Game({
     canvasDimensions: new Vec2(1024, 768),
     world: {
         dimensions: new Vec3(1024 * 100, 768 * 100, 1000000)
-    }
+    },
+    states: [
+        MAIN_STATE
+    ]
 });
-
-game.registerShader(ShaderPrograms.BASIC);
-game.registerShader(ShaderPrograms.COLOR_PER_VERTEX);
-
-game.addState(MAIN_STATE);
 
 game.start(MAIN_STATE.name);
